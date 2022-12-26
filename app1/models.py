e30f4299c78cf420991e2820694aec5ce12e99cb
Piotr Paczkowski <piterspp1998@gmail.com>
2022-12-26 20:54:28 +0100

    feat: added basic diango view of adding new robots to database

--- app1/models.py
+++ app1/models.py
@@ -4,19 +4,22 @@ from django.db import models
 from django.utils import timezone
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    def __str__(self):
-        return self.question_text
-        
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    def __str__(self):
-        return self.choice_text
+class Robot(models.Model):
+    FOUR_WHEELER = '4 wheeler'
+    AMPHIBIAN = 'amphibian'
+    TRACKED = 'tracked'
+    FLYING = 'flying'
+    ROBOT_TYPES_CHOICES = [
+        (FOUR_WHEELER, ('4 wheeler')),
+        (AMPHIBIAN, ('amphibian')),
+        (TRACKED, ('tracked')),
+        (FLYING, ('flying')),
+    ]
+    serial_number = models.CharField(max_length = 50)
+    production_date = models.DateField()
+    type = models.CharField(
+        max_length = 10,
+        choices = ROBOT_TYPES_CHOICES,
+        default = '4 wheeler'
+    )
+    company = models.CharField(max_length = 20)
--- app1/views.py
+++ app1/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Question
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    output = HttpResponse('Jazda')
-    return HttpResponse(output)
+    return HttpResponse("The site is working!")
